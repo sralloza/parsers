85ddd7c37ee6f1e082d4f423782e00da8d351cee
Diego Alloza González <sralloza@gmail.com>
2021-09-25 14:15:27 +0200

    chore: change todoist project

--- parsers/utils/todoist.py
+++ parsers/utils/todoist.py
@@ -5,5 +5,5 @@ from parsers.config import settings
 
 def add_task(msg):
     api = TodoistAPI(settings.todoist_token)
-    api.items.add(msg, project_id="2188189394", due={"string": "today"})
+    api.items.add(msg, project_id="2272212085", due={"string": "today"})
     api.commit()
