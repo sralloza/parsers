0b163967567e7624adffaf37d4d8b7d655e1856c
Diego Alloza González <sralloza@gmail.com>
2022-02-04 23:36:56 +0100

    chore: lint

--- parsers/config.py
+++ parsers/config.py
@@ -1,10 +1,16 @@
 from pydantic import BaseSettings
 
 
+OP_INDEX_URL = (
+    "https://animebymega.blogspot.com/2020/07/"
+    "one-piece-933-sub-espanol-por-mega.html"
+)
+
+
 class Settings(BaseSettings):
     telegram_bot_token: str
     telegram_chat_id: int
-    op_anime_index_url: str = "https://animebymega.blogspot.com/2020/07/one-piece-933-sub-espanol-por-mega.html"
+    op_anime_index_url: str
     todoist_token: str
     s3_bucket_name: str
 
